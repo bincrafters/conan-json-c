0a0e8922f4a6254133ce786f3e1c42a529692a11
bincrafters-user <bincrafters@gmail.com>
2019-09-01 01:45:54 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.9.2

--- conanfile.py
+++ conanfile.py
@@ -18,9 +18,9 @@ class JSONCConan(ConanFile):
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    default_options = {'shared': False, 'fPIC': True}
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -33,26 +33,26 @@ class JSONCConan(ConanFile):
         file_name = "{0}-{1}-20180305".format(self.name, self.version)
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, file_name))
         extracted_dir = self.name + "-" + file_name
-        os.rename(extracted_dir, self.source_subfolder)
-        tools.patch(base_path=self.source_subfolder, patch_file="json-c.patch")
+        os.rename(extracted_dir, self._source_subfolder)
+        tools.patch(base_path=self._source_subfolder, patch_file="json-c.patch")
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         if tools.cross_building(self.settings) and self.settings.os != "Windows":
             host = tools.get_gnu_triplet(str(self.settings.os), str(self.settings.arch))
-            tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"),
+            tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                                   "execute_process(COMMAND ./configure ",
                                   "execute_process(COMMAND ./configure --host %s " % host)
         cmake = CMake(self)
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="COPYING", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
+        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.install()
 
     def package_info(self):
